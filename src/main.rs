1c8dfa95e5ab361ef2c52bcceb30fe251ce8bc27
Kaede Fujisaki <kaede@hexe.net>
2023-09-11 04:06:20 +0900

    Use args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,63 @@
+use std::process::Command;
+
 mod tune;
 
+fn app() -> clap::Command {
+  use clap::{Command, Arg, ArgAction, value_parser};
+  Command::new("tune")
+    .author("Kaede Fujisaki")
+    .about("Tune")
+    .arg(Arg::new("verbose")
+      .long("verbose")
+      .short('v')
+      .required(false)
+      .action(ArgAction::Count)
+      .value_parser(value_parser!(u8))
+      .help("Show verbose message"))
+    .arg(Arg::new("NAME")
+      .help("tuning name")
+      .index(1)
+      .action(ArgAction::Set)
+      .value_parser(["pythagoras", "japan"])
+      .required(true))
+}
+
+fn setup_logger(log_level: log::LevelFilter) -> Result<(), fern::InitError> {
+  fern::Dispatch::new()
+    .format(|out, message, record| {
+      out.finish(format_args!(
+        "{}[{}] {}",
+        chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+        record.level(),
+        message
+      ))
+    })
+    .level(log_level)
+    .chain(std::io::stdout())
+    //.chain(fern::log_file("output.log")?)
+    .apply()?;
+  Ok(())
+}
 fn main() -> anyhow::Result<()> {
+  use log::{info, error};
+  let m = app().get_matches();
+  let log_level = match m.get_one::<u8>("verbose") {
+    None | Some(0) => log::LevelFilter::Info,
+    Some(1) => log::LevelFilter::Debug,
+    _ => log::LevelFilter::Trace,
+  };
+  setup_logger(log_level)?;
   let tuner = tune::Tuner::new();
-  tuner.tune::<tune::Japan>(440.0);
+  match m.get_one::<String>("NAME").map(|it| it.as_str()) {
+    None | Some("pythagoras") => {
+      tuner.tune::<tune::Japan>(440.0);
+    },
+    Some("japan") => {
+      tuner.tune::<tune::Japan>(440.0);
+    },
+    Some(name) => {
+      error!("Unknown name: {}", name)
+    },
+  }
   Ok(())
 }
--- src/tune.rs
+++ src/tune.rs
@@ -19,11 +19,12 @@ impl Tuner {
   }
 
   pub fn tune<T: Tune>(&self, base: f64) {
+    use log::info;
     let sounds = T::generate();
     let mut sounds = (1..=sounds.len()).zip(sounds).collect::<Vec<_>>();
     sounds.sort_by(|a, b| a.1.total_cmp(&b.1));
     for (cnt, sound) in sounds {
-      println!("{}: {}", cnt, sound * base);
+      info!("{}: {}", cnt, sound * base);
     }
   }
 }
