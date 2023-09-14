98e2c76c620d49d99e30990bbbb45eb7d66cb015
Kaede Fujisaki <kaede@hexe.net>
2023-09-14 19:59:47 +0900

    test added

--- src/tune.rs
+++ src/tune.rs
@@ -35,3 +35,15 @@ pub fn rotate<T: Default + Clone + Sized>(v: &Vec<T>, n: usize) -> Vec<T> {
   b.append(&mut Vec::from(a).iter().map(|it| it.clone()).collect());
   b
 }
+
+#[cfg(test)]
+mod test {
+  #[test]
+  fn rotate() {
+    let v = Vec::from([1,2,3]);
+    let v = super::rotate(&v, 2);
+    assert_eq!(v[0], 3);
+    assert_eq!(v[1], 1);
+    assert_eq!(v[2], 2);
+  }
+}
