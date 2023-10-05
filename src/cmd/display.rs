e929c59e3bde7b9b64eb768b91417e9195a33021
Kaede Fujisaki <kaede@hexe.net>
2023-10-05 13:52:24 +0900

    Play and out

--- src/cmd/display.rs
+++ src/cmd/display.rs
@@ -17,6 +17,8 @@ pub const TUNES: [&'static str; 5] = [
 ];
 // (2 ** (1/12)) ** -9 * 440.0
 const C4HZ: f64 = 261.6255653005985;
+const SAMPLE_RATE: usize = 48 * 1000;
+const NUM_CHANNELS: usize = 1;
 
 pub fn run(tune_name: &str) -> anyhow::Result<()> {
   info!("Let's display tune: {} tuning", tune_name);
@@ -31,13 +33,18 @@ pub fn run(tune_name: &str) -> anyhow::Result<()> {
     },
   }
 }
+
 pub fn output(name: &str, sounds: &Vec<f64>) -> anyhow::Result<()> {
+  let data = generate_sounds(sounds);
+  save_to_file(name, &data)?;
+  play(&data)?;
+  Ok(())
+}
+
+fn generate_sounds(sounds: &Vec<f64>) -> Vec<f32> {
   use std::f64::consts::PI;
-  let mut wav = Vec::<f32>::new();
-  let (_stream, handle) = rodio::OutputStream::try_default()?;
-  let sink = rodio::Sink::try_new(&handle)?;
-  let sample_rate = (48 * 1000) as usize;
-  let samples_per_sound = sample_rate * 1;
+  let mut data = Vec::<f32>::new();
+  let samples_per_sound = SAMPLE_RATE * NUM_CHANNELS;
   let num_fade = samples_per_sound / 100;
   let num_fade_half = num_fade / 2;
   let amp = 0.7;
@@ -45,11 +52,11 @@ pub fn output(name: &str, sounds: &Vec<f64>) -> anyhow::Result<()> {
   let mut max_sample: f32 = -1.0;
   let mut min_sample: f32 = 1.0;
   for _ in 0..num_fade {
-    wav.push(0.0);
+    data.push(0.0);
   }
   for hz in sounds {
     for dt in 0..samples_per_sound {
-      let x = (t + dt) as f64 / (sample_rate as f64);
+      let x = (t + dt) as f64 / (SAMPLE_RATE as f64);
       let sample = (x * hz * 2.0 * PI).sin();
       let f = if dt <= num_fade_half {
         0.0
@@ -65,16 +72,44 @@ pub fn output(name: &str, sounds: &Vec<f64>) -> anyhow::Result<()> {
       let sample = (sample * f * amp) as f32;
       max_sample = max_sample.min(sample);
       min_sample = min_sample.max(sample);
-      wav.push(sample);
+      data.push(sample);
     }
     t += samples_per_sound + num_fade;
   }
   for _ in 0..num_fade {
-    wav.push(0.0);
+    data.push(0.0);
   }
   info!("max: {} min: {}", max_sample, min_sample);
-  sink.append(RawSource::new(wav, 1, sample_rate));
-  //sink.append(rodio::source::SineWave::new(440.0));
+  data
+}
+
+fn play(data: &[f32]) -> anyhow::Result<()> {
+  let (_stream, handle) = rodio::OutputStream::try_default()?;
+  let sink = rodio::Sink::try_new(&handle)?;
+  sink.append(RawSource::new(Vec::from(data), 1, SAMPLE_RATE));
   sink.sleep_until_end();
+  Ok(())
+}
+
+fn save_to_file(name: &str, data: &[f32]) -> anyhow::Result<()> {
+  let filename = {
+    let path = std::path::Path::new(".").join("display.wav");
+    std::fs::create_dir_all(path.clone().into_os_string())?;
+    path
+      .join(name)
+      .with_extension("wav")
+  };
+
+  let spec = hound::WavSpec {
+    channels: 1,
+    sample_rate: 44100,
+    bits_per_sample: 16,
+    sample_format: hound::SampleFormat::Int,
+  };
+  let mut writer = hound::WavWriter::create(filename, spec)?;
+  for item in data {
+    writer.write_sample(item * i16::MAX as f32)?;
+  }
+  writer.finalize()?;
   Ok(())
 }
